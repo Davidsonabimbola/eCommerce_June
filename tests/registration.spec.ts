1f5407ecb270273b8a71cf48d109415e59d37714
Abimbola Davidson <davidsonmm9@gmail.com>
2025-06-13 18:18:01 +0300

    first commit

---
+++ tests/registration.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+
+export interface userDetails {
+        email : string
+        firstName : string
+        lastName : string
+        phoneNumber : string
+        password : string
+
+    }
+
+test.describe('Register new user',()=>{
+
+   
+
+test('successfully register new user', async ({ page }) => {
+
+    const newUser : userDetails ={
+        email : "timmy_004@example.com",
+        firstName : "Timmy",
+        lastName : 'Banks',
+        phoneNumber : '2359087632',
+        password : '12.Nopassword.12'
+
+    }
+     
+    await page.goto('https://rahulshettyacademy.com/client/')
+    await page.locator('a').getByText('Register here').click()
+
+    await page.locator('input[id="firstName"]').fill(newUser.firstName)
+    await page.locator('input[id="lastName"]').fill(newUser.lastName)
+    await page.locator('input[id="userEmail"]').fill(newUser.email)
+    await page.locator('input[id="userMobile"]').fill(newUser.phoneNumber)
+    const occupationDropdown = await page.locator('[formcontrolname="occupation"]')
+    await occupationDropdown.selectOption('Student')
+    await page.locator('input[id="userPassword"]').fill(newUser.password)
+    await page.locator('input[id="confirmPassword"]').fill(newUser.password)
+    await page.locator('input[type="checkbox"]').nth(0).check()
+    await page.locator('input[id="login"]').click()
+
+
+    
+  });
+
+
+})
+
